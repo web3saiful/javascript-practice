8f32292e63b573c701ad8997cea5c7acbbb566aa
web3saiful <web3saiful@gmail.com>
2025-09-18 14:16:05 +0600

    Updated my code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -739,33 +739,46 @@
 
 
 
- 
-// map in js
-const apples={name:'Apples'};
-const bananas={name:'Bananas'};
-const oranges={name:'Oranges'};
+// set in js
+// const apples={name:'Apples'};
+// const bananas={name:'Bananas'};
+// const oranges={name:'Oranges'};
 
-    // create a Map
-const fruits=new Map();
+//     // create a Map
+// const fruits=new Map();
 
-   // add map Elements to the map
-fruits.set(apples,500);
-fruits.set(bananas,300);
-fruits.set(oranges,200);
+//    // add map Elements to the map
+// fruits.set(apples,500);
+// fruits.set(bananas,300);
+// fruits.set(oranges,200);
 
-console.log(fruits.get(apples));
-
-
+// console.log(fruits.get(apples)); 
+// // m
 
 
 
-funcion
+
+
+// funcion
 const a=function(){
-    c=5;
+    let c=5;
     return c;
 }
 
 console.log(a());
+
+
+
+
+
+
+
+
+// self-invoking function
+function(){
+    console.log("I am a ")
+}
+
 
 
 
