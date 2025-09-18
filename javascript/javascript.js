f27911f492518b9de110a34203f3c9d142ce9c28
web3saiful <web3saiful@gmail.com>
2025-09-18 12:00:31 +0600

    Updated my code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -720,16 +720,45 @@
  
 
 
-const letters=new Set(['a','b','c']);
 
-const lettersIterator =letters.values();
 
-for (let a of lettersIterator){
-    console.log(a);
-}
+
+
+
+
+// set in js
+// const letters=new Set(['a','b','c']);
+
+// const lettersIterator =letters.values();
+
+// for (let a of lettersIterator){
+//     console.log(a);
+// }
+
+
+
 
 
  
+// map in js
+const apples={name:'Apples'};
+const bananas={name:'Bananas'};
+const oranges={name:'Oranges'};
+
+    // create a Map
+const fruits=new Map();
+
+   // add map Elements to the map
+fruits.set(apples,500);
+fruits.set(bananas,300);
+fruits.set(oranges,200);
+
+console.log(fruits.get(apples));
+
+
+
+
+
 
 
 
