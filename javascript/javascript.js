8c9c6dcc06b313803b874675726990f22fb10390
web3saiful <web3saiful@gmail.com>
2025-09-19 23:41:42 +0600

    Updated my code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -851,7 +851,6 @@
 
 
 
-
 function a(x){
     x.one=7;
     return x.one*x.two;
@@ -871,6 +870,20 @@ console.log();
 
 
 
+
+function a(x){
+    x.one=7;
+    return x.one*x.two;
+}
+
+let m ={
+    one:4,
+    two:5
+};
+
+console.log(a(m));
+
+console.log();
 
 
 
