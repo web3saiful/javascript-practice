953ba2a9ee05e2f558a9b55f4833e4fac96bc2ff
web3saiful <web3saiful@gmail.com>
2025-10-03 17:35:50 +0600

    Updated my code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1458,29 +1458,93 @@
 
 
 
-// Promise
-const status=true;
+// // Promise
+// const status=true;
 
-console.log('Task 1');
-// promise defination
-const promise=new Promise(function(resolve,reject){
-    setTimeout(function(){
-   if(status){
-     resolve('Task 2');
+// console.log('Task 1');
+// // promise defination
+// const promise=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//    if(status){
+//      resolve('Task 2');
+//    }else{
+//        reject('failed');
+//    } 
+   
+//   },2000);
+// });
+// // promise call
+//  promise
+//         .then(function(value){
+//           console.log(value);
+//         })
+//         .catch(function(err){
+//           console.log(err)
+//         });
+        
+
+// console.log('Task 3');
+
+
+
+
+
+
+
+
+// Promise
+const paymentSuccess=true;
+const marks=((98));
+
+function enroll(){
+     console.log('Course enrollment is in progress.');
+     const promise=new Promise(function(resolve,reject){
+          setTimeout(function(){
+   if(paymentSuccess){
+     resolve(paymentSuccess);
    }else{
-       reject('failed');
+       reject('Payment failed');
    } 
    
   },2000);
-});
-// promise call
- promise
-        .then(function(value){
-          console.log(value);
-        })
-        .catch(function(err){
-          console.log(err)
-        });
-        
+     });
+     return promise;
+}
 
-console.log('Task 3');
+function progress(){
+console.log('Course on progress...');
+
+const promise=new Promise(function(resolve,reject){
+  setTimeout(function(){
+    if(marks>=80){
+      resolve();
+    }else{
+      reject('you could not get enough marks to get certifiate');
+    }
+  },3000)
+})
+
+   return promise
+}
+   
+function getCertificate(){
+  console.log('Preparing your certificate!');
+
+  const promise=new Promise(function(resolve){
+    setTimeout(function(){
+      resolve("Congrate!YOU got the certificate");
+    },1000);
+  });
+
+   return promise;
+}
+
+enroll()
+     .then(progress)
+     .then(getCertificate)
+     .then(function(value){
+      console.log(value);
+     })
+     .catch(function(err){
+      console.log(err)
+     });
