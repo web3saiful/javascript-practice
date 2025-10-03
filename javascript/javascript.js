52d470a68ea06f45d2e352847770817b5377f5ca
web3saiful <web3saiful@gmail.com>
2025-10-03 12:32:23 +0600

    Updated my code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1462,17 +1462,18 @@
 const status=true;
 
 console.log('Task 1');
-
+// promise defination
 const promise=new Promise(function(resolve,reject){
     setTimeout(function(){
    if(status){
      resolve('Task 2');
    }else{
        reject('failed');
-   }
+   } 
    
   },2000);
 });
+// promise call
  promise
         .then(function(value){
           console.log(value);
