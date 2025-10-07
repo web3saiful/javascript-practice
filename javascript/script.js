442715a5aef4b58bb489e98bf6cbc74994ed6c61
web3saiful <web3saiful@gmail.com>
2025-10-07 21:26:59 +0600

    Updated my code

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,27 @@
-const element=document.getElementById('demo');
+// const element=document.getElementById('demo');
 
-element.innerHTML=`Today's date is : ${Date()}`;
+// element.innerHTML=`Today's date is : ${Date()}`;
 
 
+
+
+
+// function validateForm(){
+//     const form=document.forms['myForm'];
+//     console.log (form['fname']);
+
+//     return false;
+// }
+
+
+
+
+
+// const x = document.forms["frm1"];
+// console.log(x);
+// let text = "";                                   
+
+
+
+
+const x=document.forms["myForm"];
