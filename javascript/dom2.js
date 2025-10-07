3150565cf68c957a7ef73bb015077bd979ba8061
web3saiful <web3saiful@gmail.com>
2025-10-07 18:40:20 +0600

    Updated my code

--- javascript/dom2.js
+++
@@ -1,3 +0,0 @@
-const element=document.getElementById('myImage');
-element.src="test.png";
-
---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+const element=document.getElementById('demo');
+
+element.innerHTML=`Today's date is : ${Date()}`;
+
+
