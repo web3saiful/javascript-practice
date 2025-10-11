383be0d9fb273363da068e458a689abea0d82b77
web3saiful <web3saiful@gmail.com>
2025-10-11 20:43:25 +0600

    Added all new and modified files

---
+++ problem-solving.js
@@ -0,0 +1,6 @@
+How can we print numbers from one to six randomly in ludo game?
+
+function randomNumber(min,max){
+	return Math.floor(Math.random()*(max-min+1))+min;
+}
+console.log(getRandomNumber(1,6));
