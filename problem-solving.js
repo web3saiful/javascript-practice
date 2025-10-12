a8a062c1b85145061ca7ac8504fd38ecb55b96c2
web3saiful <web3saiful@gmail.com>
2025-10-12 20:50:29 +0600

    Updated my code

--- problem-solving.js
+++ problem-solving.js
@@ -46,27 +46,24 @@
 
 
 
-// how to indicate how many vowel are in the sentanc?
-const vowels=["a","e","i","o","u","A","E","I","o","U"];
+// // how to indicate how many vowel are in the sentanc?
+// const vowels=["a","e","i","o","u","A","E","I","o","U"];
 
 
-function countVowels(sentence){
-	let count=0;
-	const letters=Array.from(sentence);
+// function countVowels(sentence){
+// 	let count=0;
+// 	const letters=Array.from(sentence);
 
-	letters.forEach(function(value){
-		if(vowels.includes(value)){
-			count++;
-		}
-	});
+// 	letters.forEach(function(value){
+// 		if(vowels.includes(value)){
+// 			count++;
+// 		}
+// 	});
 
-	return  count; 
+// 	return  count; 
 
-}
-console.log(countVowels("Hello World"));
-
-
-
+// }
+// console.log(countVowels("Hello World"));
 
 
 
@@ -74,18 +71,30 @@ console.log(countVowels("Hello World"));
 
 
 
-// How can i remove duplicate numbers from an arry?
-const numbers=[1,4,5,6,7,6,8,9,10,4];
-
-const duplicate=numbers.filter(function(value,index,array){
-	return array.indexOf(value)===index
-});
-
-console.log(duplicate);
 
 
 
+// // How can i remove duplicate numbers from an arry?
+// const numbers=[1,4,5,6,7,6,8,9,10,4];
+
+// const duplicate=numbers.filter(function(value,index,array){
+// 	return array.indexOf(value)!==index
+// });
+
+// console.log(duplicate);
 
 
 
 
+
+
+
+
+
+// how many times is the word Sumit used in the following sentence?
+// In what number possition was Sumit found for the first time?
+
+const sentance="Learn with Sumit is all about teaching web development skills in an efficient and practical manner. If you are just getting started in web development, Learn with Sumit has all the tools you need to learn the newest and most popular technologies to convert you from a no stack to full stack developer. Learn with Sumit also deep dives into advanced topics using the latest best practices for you seasoned web developers.";
+
+const matches=sentance.match(/sumit/gi);
+console.log(matches);
