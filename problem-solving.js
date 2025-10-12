4a84c570e5e3ebe745279be38bb088d3cbfbf54d
web3saiful <web3saiful@gmail.com>
2025-10-12 13:30:44 +0600

    Updated my code

--- problem-solving.js
+++ problem-solving.js
@@ -1,17 +1,29 @@
-// How can we print numbers from one to six randomly in ludo game?
+// // How can we print numbers from one to six randomly in ludo game?
 
-function randomNumber(min,max){
-	return Math.floor(Math.random()*(max-min+1))+min;
-}
-console.log(randomNumber(1,6));
-
-
+// function randomNumber(min,max){
+// 	return Math.floor(Math.random()*(max-min+1))+min;
+// }
+// console.log(randomNumber(1,6));
 
 
 
 
 
-// `How can we arrage everyone's names alphabetically in our classroom?`
+
+
+// // `How can we arrage everyone's names alphabetically in our classroom?`
+
 // const students=["Sumit","Saad","Minhaj","amdad sir","brather"];
 
 // console.log(students.sort());
+
+
+
+
+
+
+const roll_numbers=[8,3,5,1,4,6,2,7];
+
+console.log(roll_numbers.sort(function(a,b){
+	return b-a;
+}))
