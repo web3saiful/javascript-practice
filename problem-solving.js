ccc63075c2edafa7e134e4c5fa1cdbff60173178
web3saiful <web3saiful@gmail.com>
2025-10-12 20:53:23 +0600

    Updated my code

--- problem-solving.js
+++ problem-solving.js
@@ -97,4 +97,6 @@
 const sentance="Learn with Sumit is all about teaching web development skills in an efficient and practical manner. If you are just getting started in web development, Learn with Sumit has all the tools you need to learn the newest and most popular technologies to convert you from a no stack to full stack developer. Learn with Sumit also deep dives into advanced topics using the latest best practices for you seasoned web developers.";
 
 const matches=sentance.match(/sumit/gi);
-console.log(matches);
+const occurances=matches.length;
+
+console.log(occurances);
