73cc37f42b3b5b13f5c4fd42fb6a4eed8a06f43c
web3saiful <web3saiful@gmail.com>
2025-10-12 20:23:28 +0600

    Updated my code

--- problem-solving.js
+++ problem-solving.js
@@ -78,7 +78,7 @@ console.log(countVowels("Hello World"));
 const numbers=[1,4,5,6,7,6,8,9,10,4];
 
 const duplicate=numbers.filter(function(value,index,array){
-	return array.indexOf(value)!==index
+	return array.indexOf(value)===index
 });
 
 console.log(duplicate);
